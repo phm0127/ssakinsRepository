6373c840527ad11a6f0556fa5a76e734b0a2faa9
phm0127 <phm0127@gmail.com>
2020-10-26 10:14:15 +0000

    skip password

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 echo "["`date`"] start to install SSAKINS."
 sleep 1
 
-echo "["`date`"]" `sudo docker pull phm0127/ssakins`
+echo "["`date`"] pull SSAKINS image from docker-hub... " 
+sudo docker pull phm0127/ssakins
 sleep 1
 
-echo -n "["`date`"] docker SSAKINS image pulling... "
+echo -n "["`date`"] Set environments for SSAKINS ... "
 
 sp='/-\|'
 printf ' '
@@ -14,7 +15,12 @@ for n in `seq 0 10` ; do
     sp=${sp#?}${sp%???}
 done
 echo ""
-sudo docker run --name "ssakins" -v ./ssakins_home/:/var/jenkins_home/ -p 8000:8080 -d phm0127/ssakins
+SSAKINS_HOME=`pwd`
+sudo chown 1000 $SSAKINS_HOME/ssakins_home
+echo "set SSAKINS_HOME directory owned by root."
+sleep 1
+
+sudo docker run --name "ssakins" -u 'root' -v $SSAKINS_HOME/ssakins_home/:/var/jenkins_home/ -p 8000:8080 -d phm0127/ssakins
 echo -n "["`date`"] run SSAKINS on docker... "
 
 printf ' '
